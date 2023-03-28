57398d45d8ff250ae6249005bb1469bd939f97d9
Dell <ersadehmedov01@gmail.com>
2023-03-29 01:28:51 +0300

    Add project files.

---
+++ gameTask/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+
+namespace gameTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //task1
+            //int integerVariable = 10;
+            //double doubleVariable = 3.14;
+            //string stringVariable = "How are you!";
+            //bool booleanVariable = true;
+            //char charVariable = '!';
+
+
+            //Console.WriteLine("integerVariable: " + integerVariable);
+            //Console.WriteLine("doubleVariable: " + doubleVariable);
+            //Console.WriteLine("stringVariable: " + stringVariable);
+            //Console.WriteLine("booleanVariable: " + booleanVariable);
+            //Console.WriteLine("charVariable: " + charVariable);
+
+            //Console.ReadLine();
+
+
+
+            //task2
+            //int number = 10;
+            //Console.WriteLine("int namber varible: " + number);
+
+            //float floatsum = (float)number;
+            //Console.WriteLine("float number varible: " + floatsum);
+
+            //Console.ReadLine();
+
+
+
+
+
+            //task3
+            //Console.WriteLine("enter a number");
+            //int num = Convert.ToInt32(Console.ReadLine());
+
+            //if (num > 0)
+            //{
+            //    Console.WriteLine("enter number positive");
+            //}
+            //else if (num < 0)
+            //{
+            //    Console.WriteLine("enter number negative");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("enter number zero");
+            //}
+
+
+
+
+            //task4
+            //Console.WriteLine("Yasini daxil et");
+            //int num=Convert.ToInt32(Console.ReadLine());
+
+            //if (num >= 60)
+            //{
+            //    Console.WriteLine("Qocalmisan Get Pensiyanin Dadini Cixart");
+            //}
+            //else if(num <= 60)
+            //{
+            //    Console.WriteLine("Hele qocalmiyibsan cavanlara dash cixarsarsan ");
+            //}
+
+
+            //task5
+            //for (int i = 2; i <=100; i+=2)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+
+            //task6
+            //Console.WriteLine("Ove enter in number");
+            //int num = Convert.ToInt32(Console.ReadLine());
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.WriteLine(num + "x" + "=" + num * i);
+            //}
+
+
+
+            //task7
+            //string password = "dark001001";
+            //string guess;
+
+            //do
+            //{
+            //    Console.Write("Zehmet deyilse shifreni girerdin: ");
+            //    guess = Console.ReadLine();
+
+            //    if (guess != password)
+            //    {
+            //        Console.WriteLine("O Dölüü .");
+            //    }
+            //} while (guess != password);
+
+            //Console.WriteLine("Düz deyirsen");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}   
